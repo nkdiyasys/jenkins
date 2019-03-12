099627490964c37853b38220215164746e373210
Nithin K <nithin.k@glowtouch.com>
2019-03-12 12:08:52 +0530

    Initial commit

---
+++ JenkinsSample/JenkinsSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsSample
+//
+//  Created by Nithin on 12/03/19.
+//  Copyright © 2019 Nithin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
